9bc0128cc689477150f783e4ca96b0807f33f17e
Young-Gi Park <ghostyak@gmail.com>
2026-08-22 21:23:53 +0900

    Build Boxes product landing page

---
+++ src/data/products.ts
@@ -0,0 +1,10 @@
+export const boxes = {
+	name: 'Ghostyak Boxes',
+	version: '0.3.21',
+	platform: 'Windows 10/11',
+	fileSize: '약 3.1MB',
+	requirement: 'Microsoft Edge WebView2 Runtime 필요',
+	downloadUrl:
+		'https://github.com/ghostyak/boxes/releases/download/v0.3.21/GhostyakBoxes-0.3.21-setup.exe',
+	description: 'Windows 바탕화면의 파일과 바로가기를 박스 단위로 묶어 정리하는 데스크톱 도구',
+};
